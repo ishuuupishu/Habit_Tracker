96219435a75a51bc1622d59a9794b87d0df4a262
ishuuupishu <ishuenii134@gmail.com>
2025-09-02 21:11:31 +0600

    Fixed Firebase Auth and Firestore configuration; resolved PERMISSION_DENIED errors

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,18 @@
+// Top-level build.gradle.kts
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        
+        classpath("com.google.gms:google-services:4.3.15")
+        classpath("com.android.tools.build:gradle:8.1.0") 
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,14 +20,13 @@ allprojects {
     }
 }
 
+// Flutter custom build directory handling
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
     project.evaluationDependsOn(":app")
 }
 
